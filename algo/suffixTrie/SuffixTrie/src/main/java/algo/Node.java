3b3eae972e938bf0dad803330c74c2f5cf37da50
Nikolai Perlt <nikolaiperlt@gmail.com>
2020-04-14 14:06:25 +0200

    added comments to code

--- algo/suffixTrie/SuffixTrie/src/main/java/algo/Node.java
+++ algo/suffixTrie/SuffixTrie/src/main/java/algo/Node.java
@@ -5,7 +5,7 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Node {
+class Node {
   private char letter;
   private List<Node> children = new LinkedList<>();
   private Collection<String> words = new HashSet<>();
@@ -16,24 +16,34 @@ public class Node {
     this.layer = layer;
   }
 
+  /**
+   * @param subString
+   * @param wholeWord Adds a child to our node
+   */
   public void addChild(String subString, String wholeWord) {
-    if (subString.length() < 1) {
+    // if the given substring is null return from method
+    if (subString == null) {
       return;
     }
 
+    // Create new substring with the first letter removed
     subString = subString.substring(1);
 
-    if (!this.words.contains(wholeWord)) {
-      this.words.add(wholeWord);
-    }
+    // Checks if wholeWord is already saved in this node. We use a hashset to save
+    // time doing this opration
+    this.words.add(wholeWord);
 
+    // This is the exit condition and ends our recursion
     if (subString.equals("")) {
       return;
     }
 
+    // we find the first letter in out substring
     char letter = subString.charAt(0);
     boolean found = false;
 
+    // Iterate out children and checks if it has a child with the same letter as our
+    // variable 'letter'
     for (Node node : this.children) {
       if (node.getLetter() == letter) {
         node.addChild(subString, wholeWord);
@@ -42,16 +52,26 @@ public class Node {
     }
 
     if (!found) {
+      // if the saved words in the node is larger then 2, we can just create a new
+      // node with the next letter
       if (this.words.size() > 2) {
         Node node = new Node(letter, layer + 1);
         children.add(node);
         node.addChild(subString, wholeWord);
       } else if (this.words.size() > 1) {
+        // else we have to handle a collition where the smallest word get handled first
+        // (springer buk)
         String orginalWord = this.words.iterator().next();
         String orginalWordSubstring = orginalWord.substring(this.layer);
 
         Node node = new Node(letter, layer + 1);
         children.add(node);
+
+        // as the layer grow the orginal substring will be empty therefore we set it to
+        // null
+        if (orginalWordSubstring.length() <= this.layer) {
+          orginalWordSubstring = null;
+        }
 
         if (orginalWord.length() > wholeWord.length()) {
           node.addChild(subString, wholeWord);
@@ -62,22 +82,33 @@ public class Node {
         }
       }
     }
-
   }
 
+  /**
+   * @param substring
+   * @return Collection<String> Find a given substring in this node
+   */
   public Collection<String> find(String substring) {
+    // removes the first letter and checks if string is not empty
     String tempString = substring.substring(1);
 
     if (tempString.equals("")) {
       return this.words;
     }
+
+    // saves the first letter
     char letter = tempString.charAt(0);
 
+    // iterate our children list and checks if we can find a child with the given
+    // letter and then call that childs find method recursively
     for (Node node : this.children) {
       if (node.getLetter() == letter) {
         return node.find(tempString);
       }
     }
+
+    // if we cant find a child node we have to see if one of the words in this node
+    // contains the substring because we use a compact trie
     LinkedList<String> resList = new LinkedList<>();
     for (String word : this.words) {
       if (word.contains(substring)) {
@@ -88,10 +119,16 @@ public class Node {
 
   }
 
+  /**
+   * @return List<Node>
+   */
   public List<Node> getChildren() {
     return children;
   }
 
+  /**
+   * @return char
+   */
   public char getLetter() {
     return letter;
   }
--- algo/suffixTrie/SuffixTrie/src/main/java/algo/SuffixTrie.java
+++ algo/suffixTrie/SuffixTrie/src/main/java/algo/SuffixTrie.java
@@ -8,19 +8,27 @@ public class SuffixTrie {
 
   private List<Node> Nodes = new LinkedList<>();
 
-  public static String wholeWord;
-
+  /**
+   * @param word Adds a word to our datastructur
+   */
   public void add(String word) {
+    // makes word lowercase
     word = word.toLowerCase();
-    SuffixTrie.wholeWord = word;
 
+    // makes sure we use all suffixes of the word
     for (int i = 0; i < word.length(); i++) {
+      // Create a substring for each suffix
       String substring = word;
+      // if the word is longer then 1 subtract the the first letter
       if (word.length() != 1) {
         substring = word.substring(i);
       }
+      // Takes the first letter in our substring and saves it in letter
       char letter = substring.charAt(0);
 
+      // Iterates each child node and see if we can find the node with the same letter
+      // as 'letter'
+      // we then call that nodes addChild method
       boolean found = false;
       for (Node Node : Nodes) {
         if (Node.getLetter() == letter) {
@@ -28,7 +36,7 @@ public class SuffixTrie {
           found = true;
         }
       }
-     
+      // If we cant find that node we create a new node with the given letter
       if (!found) {
         Node newNode = new Node(letter, 1);
         this.Nodes.add(newNode);
@@ -37,11 +45,17 @@ public class SuffixTrie {
     }
   }
 
+  /**
+   * @param substring
+   * @return Collection<String> Finds a given substring on out word collection
+   */
   public Collection<String> find(String substring) {
+    // We find the first letter and checks if we can find a node with the given
+    // letter in out child list
     char rootLetter = substring.charAt(0);
     for (Node Node : Nodes) {
-      if(Node.getLetter() == rootLetter){
-       return Node.find(substring);
+      if (Node.getLetter() == rootLetter) {
+        return Node.find(substring);
       }
     }
     return new LinkedList<>();
